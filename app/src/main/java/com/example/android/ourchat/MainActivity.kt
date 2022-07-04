6b84d461eb00262f6bb9a2f523135e458cbaae91
Rocky <praneethbhargav44@gmail.com>
2022-07-04 12:41:18 +0530

    FirstCommit

---
+++ app/src/main/java/com/example/android/ourchat/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.android.ourchat
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.android.ourchat.ui.theme.OURCHATTheme
+import io.getstream.chat.android.client.ChatClient
+import io.getstream.chat.android.client.logger.ChatLogLevel
+import io.getstream.chat.android.client.models.User
+import io.getstream.chat.android.compose.ui.channels.ChannelsScreen
+import io.getstream.chat.android.compose.ui.theme.ChatTheme
+import io.getstream.chat.android.offline.model.message.attachments.UploadAttachmentsNetworkType
+import io.getstream.chat.android.offline.plugin.configuration.Config
+import io.getstream.chat.android.offline.plugin.factory.StreamOfflinePluginFactory
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // 1 - Set up the OfflinePlugin for offline storage
+        val offlinePluginFactory = StreamOfflinePluginFactory(
+            config = Config(
+                backgroundSyncEnabled = true,
+                userPresence = true,
+                persistenceEnabled = true,
+                uploadAttachmentsNetworkType = UploadAttachmentsNetworkType.NOT_ROAMING,
+            ),
+            appContext = applicationContext,
+        )
+        // 2 - Set up the client for API calls and with the plugin for offline storage
+        val client = ChatClient.Builder("b67pax5b2wdq", applicationContext)
+            .withPlugin(offlinePluginFactory)
+            .logLevel(ChatLogLevel.ALL) // Set to NOTHING in prod
+            .build()
+        // 3 - Authenticate and connect the user
+        val user = User(
+            id = "tutorial-droid",
+            name = "Tutorial Droid",
+            image = "https://bit.ly/2TIt8NR"
+        )
+        client.connectUser(
+            user = user,
+            token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidHV0b3JpYWwtZHJvaWQifQ.NhEr0hP9W9nwqV7ZkdShxvi02C5PR7SJE7Cs4y7kyqg"
+        ).enqueue()
+        setContent {
+            ChatTheme {
+                ChannelsScreen(
+                    title = stringResource(id = R.string.app_name),
+                    isShowingSearch = true,
+                    onItemClick = { channel ->
+                        // TODO Start Messages Activity
+                    },
+                    onBackPressed = { finish() }
+                )
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/android/ourchat/fragments/BindingFragment.kt
@@ -0,0 +1,30 @@
+package com.example.android.ourchat.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+//import dagger.hilt.android.AndroidEntryPoint
+import kotlin.text.Typography.dagger
+
+abstract class BindingFragment<out T : ViewBinding> : Fragment() {
+
+    private var _binding: ViewBinding? = null
+    @Suppress("UNCHECKED_CAST")
+    protected val binding: T
+        get() = _binding as T
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = bindingInflater(inflater)
+        return _binding!!.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    protected abstract val bindingInflater: (LayoutInflater) -> ViewBinding
+}
